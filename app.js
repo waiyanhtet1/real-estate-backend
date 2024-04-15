ae50e8844d648d14876c08eab529d06f760ac08e
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-15 15:47:08 +0630

    setup: cors crendial in server

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoute = require("./routes/auth.route");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ credentials: true }));
 
 app.use("/api/auth", authRoute);
 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,7 +21,7 @@ const register = async (req, res) => {
       .status(201)
       .json({ message: "User created successfully!", user: newUser });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Fail to register!" });
   }
 };
 
