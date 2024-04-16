2267ea79844ba729ddd99504e74aea16aabc9f2a
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-16 12:59:15 +0630

    setup: middleware controller funcition

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -17,6 +17,7 @@ const register = async (req, res) => {
         password: hashPassword,
       },
     });
+
     res
       .status(201)
       .json({ message: "User created successfully!", user: newUser });
@@ -47,13 +48,15 @@ const login = async (req, res) => {
     expiresIn: age,
   });
 
+  const { password: userPassword, ...userInfo } = user;
+
   res
     .cookie("token", token, {
       httpOnly: true,
       maxAge: age,
     })
     .status(200)
-    .json({ message: "Login Successfully!" });
+    .json({ message: "Login Successfully!", userInfo });
 };
 
 const logout = (req, res) => {
---
+++ middleware/requireAuth.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+
+const requireAuth = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) res.status(401).json({ message: "Authorization token required" });
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) res.status(403).json({ message: "Token is not valid" });
+    req.userId = payload.id;
+
+    next();
+  });
+};
